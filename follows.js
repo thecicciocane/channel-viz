d5eeb30745cf4437237dec1e05aa2e1b78123de5
thecicciocane <thecicciocane@libero.it>
2015-03-21 18:00:47 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= 'fEclekukpCMio3xCKdNIvG8XbKXWiYefpgGbAbG06XQUn9Tf', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1674122958], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Arduino BOX', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
